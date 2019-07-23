18052b870050dc7c758083a4e5f9c56c9a885c3b
Saul Avila <saul_harry_@hotmail.com>
2019-07-23 14:31:33 -0500

    se agrega configuracion inicial del servidor eureka

--- src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioEurekaServerApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioEurekaServerApplication.java
@@ -2,7 +2,12 @@ package com.formacionbdi.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/*
+ * Con esta anotación habilitas el servidor de eureka
+ */
+@EnableEurekaServer
 @SpringBootApplication
 public class SpringbootServicioEurekaServerApplication {
 
