1f8f69fbed3f918b93c69b1c8efc5f4d1da3b834
Saul Avila <saul_harry_@hotmail.com>
2019-07-23 14:20:13 -0500

    commit inicial

---
+++ src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.springboot.app.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicioEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioEurekaServerApplication.class, args);
+	}
+
+}
