6d140f41538f87f279daeb0f07e5c2dc511fcfea
Saul Avila <saul_harry_@hotmail.com>
2019-07-23 18:31:49 -0500

    se cambia el nombre del paquete

--- src/main/java/com/formacionbdi/springboot/app/eureka/SpringbootServicioEurekaServerApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/eureka/SpringbootServicioEurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.formacionbdi.springboot.app.item;
+package com.formacionbdi.springboot.app.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
